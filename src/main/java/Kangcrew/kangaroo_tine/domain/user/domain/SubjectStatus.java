e14fb76b2ad92467d03f4d8d42c1657cd6d84515
hyowon <ally0204@naver.com>
2025-07-13 23:06:53 +0900

    [FEAT/#7] 유저 entity 및 기본 세팅

---
+++ src/main/java/Kangcrew/kangaroo_tine/domain/user/domain/SubjectStatus.java
@@ -0,0 +1,5 @@
+package Kangcrew.kangaroo_tine.domain.user.domain;
+
+public enum SubjectStatus {
+    ACTIVE, INACTIVE
+}
---
+++ src/main/java/Kangcrew/kangaroo_tine/domain/user/domain/entitiy/Subject.java
@@ -0,0 +1,54 @@
+package Kangcrew.kangaroo_tine.domain.user.domain.entitiy;
+
+import Kangcrew.kangaroo_tine.domain.user.domain.SubjectStatus;
+import Kangcrew.kangaroo_tine.global.common.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Subject extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "guardian_id", nullable = false)
+    private Guardian guardian;
+
+    @Column(length = 7, nullable = false)
+    private String connectCode;
+
+    @Column(length = 20, nullable = false)
+    private String guardianName;
+
+    @Column(length = 15, nullable = false)
+    private String phone;
+
+    @Column(length = 20, nullable = false)
+    private String name;
+
+    @Column(length = 30)
+    private String email;
+
+    private String profileImg;
+
+    private LocalDate birth;
+
+    private Boolean pushNotification;
+
+    @Enumerated(EnumType.STRING)
+    private SubjectStatus status;
+
+    private Boolean isDeleted;
+}
